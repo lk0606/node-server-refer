8f90c86ad9dd3aa847cf114ff07be58f60bcf30a
liukun09 <liukun09@qianxin.com>
2022-03-24 18:17:37 +0800

    test: 部署测试

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+
+ip=$1
+host=$2
+password=$3
+
+
+if [ -d "./dist" ]; then
+    pwd
+    ls -a
+    set user root
+    set ip 49.234.7.238
+    set password lk452123+
+    scp -r ./dist /server_root/node_server_refer
+    fi
+
+
+# else
+#     cd $project_name
+#     if [ ! -d $branch_name ]; then
+#         mkdir $branch_name
+#         cp -r $source_dir/* $target_dir/$project_name/$branch_name
+#     else
+#         cp -r $source_dir/* $target_dir/$project_name/$branch_name
+#     fi
+# fi
